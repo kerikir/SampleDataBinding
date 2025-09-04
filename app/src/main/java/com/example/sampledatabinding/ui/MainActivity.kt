fdc72652f905c39d8a6c8e5a9108da9b621b21e1
kerikir <vlasov-kerik@mail.ru>
2025-09-04 22:24:37 +0300

    add initialize profile icon on label

--- app/src/main/java/com/example/sampledatabinding/ui/MainActivity.kt
+++ app/src/main/java/com/example/sampledatabinding/ui/MainActivity.kt
@@ -1,5 +1,8 @@
 package com.example.sampledatabinding.ui
 
+import android.content.Context
+import android.content.res.ColorStateList
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageView
@@ -7,10 +10,13 @@ import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.widget.ImageViewCompat
 import androidx.lifecycle.ViewModelProvider
 import com.example.sampledatabinding.R
+import com.example.sampledatabinding.data.Popularity
 import com.example.sampledatabinding.data.SimpleViewModel
 
 class MainActivity : AppCompatActivity()
@@ -26,6 +32,7 @@ class MainActivity : AppCompatActivity()
 
         // Явно указываем начальные значения
         updateName()
+        updateLikes()
     }
 
 
@@ -45,12 +52,42 @@ class MainActivity : AppCompatActivity()
 
 
     //
-    private fun updateLikes()
+    private fun updateLikes() : Unit
     {
-        findViewById<TextView>(R.id.label_likes).text = viewModel.counterLikes.toString()
+        findViewById<TextView>(R.id.plain_likes).text = viewModel.counterLikes.toString()
         findViewById<ProgressBar>(R.id.progressBar).progress = (viewModel.counterLikes * 100 / 5)
             .coerceAtMost(100)
 
         val image: ImageView = findViewById<ImageView>(R.id.imageView)
+        val color: Int = getColor(this, viewModel.popularity)
+
+        ImageViewCompat.setImageTintList(image, ColorStateList.valueOf(color))
+        image.setImageDrawable(getImage(this, viewModel.popularity))
+    }
+
+
+    // Получение цвета в зависимости от категории популярности
+    fun getColor(context: Context, popularity: Popularity) : Int
+    {
+        return when (popularity)
+        {
+            Popularity.NORMAL -> context.theme.obtainStyledAttributes(
+                intArrayOf(android.R.attr.colorForeground)).getColor(0, 0x000000)
+
+            Popularity.POPULAR -> ContextCompat.getColor(context, R.color.popular)
+            Popularity.STAR -> ContextCompat.getColor(context, R.color.star)
+        }
+    }
+
+
+    // Получение иконки в зависимости от категории популярности
+    fun getImage(context: Context, popularity: Popularity) : Drawable?
+    {
+        return when (popularity)
+        {
+            Popularity.NORMAL -> ContextCompat.getDrawable(context, R.drawable.icon_person)
+            Popularity.POPULAR -> ContextCompat.getDrawable(context, R.drawable.icon_fire)
+            Popularity.STAR -> ContextCompat.getDrawable(context, R.drawable.icon_fire)
+        }
     }
 }
