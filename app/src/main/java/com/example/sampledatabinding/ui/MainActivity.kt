989d340916c80bd66941239a9d056fd6a9f5c85a
kerikir <vlasov-kerik@mail.ru>
2025-09-04 21:46:08 +0300

    add onClick method and initialize text view from layout

--- app/src/main/java/com/example/sampledatabinding/ui/MainActivity.kt
+++ app/src/main/java/com/example/sampledatabinding/ui/MainActivity.kt
@@ -1,21 +1,55 @@
 package com.example.sampledatabinding.ui
 
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import android.widget.ProgressBar
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.ViewModelProvider
 import com.example.sampledatabinding.R
+import com.example.sampledatabinding.data.SimpleViewModel
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+class MainActivity : AppCompatActivity()
+{
+    private val viewModel by lazy {
+        ViewModelProvider(this).get(SimpleViewModel::class.java)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        // Явно указываем начальные значения
+    }
+
+
+    // Бизнес логика
+    fun onLike(view: View) : Unit
+    {
+        viewModel.onLike()
+    }
+
+
+    // Установка значений в макете
+    private fun updateName() : Unit
+    {
+        findViewById<TextView>(R.id.plain_name).text = viewModel.name
+        findViewById<TextView>(R.id.plain_lastName).text = viewModel.lastName
+    }
+
+
+    // 
+    private fun updateLikes()
+    {
+        findViewById<TextView>(R.id.label_likes).text = viewModel.counterLikes.toString()
+        findViewById<ProgressBar>(R.id.progressBar).progress = (viewModel.counterLikes * 100 / 5)
+            .coerceAtMost(100)
+
+        val image: ImageView = findViewById<ImageView>(R.id.imageView)
     }
 }
