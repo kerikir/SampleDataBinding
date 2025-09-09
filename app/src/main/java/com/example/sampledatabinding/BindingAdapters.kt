ab7b27ba0fecd8ae45e4814b7c7fb422caa652ad
kerikir <vlasov-kerik@mail.ru>
2025-09-09 22:03:07 +0300

    add hide progress bar on start app

---
+++ app/src/main/java/com/example/sampledatabinding/BindingAdapters.kt
@@ -0,0 +1,13 @@
+package com.example.sampledatabinding
+
+import android.view.View
+import androidx.databinding.BindingAdapter
+
+class BindingAdapters
+{
+    @BindingAdapter("app:hideIfZero")
+    fun hideIfZero(view: View, number: Int)
+    {
+        view.visibility = if (number == 0) View.GONE else View.VISIBLE
+    }
+}
