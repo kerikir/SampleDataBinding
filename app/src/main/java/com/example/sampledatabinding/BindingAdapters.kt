cdaf6215ab2d61e8f10f049cce7ba2e112f04bd8
kerikir <vlasov-kerik@mail.ru>
2025-09-14 12:52:17 +0300

    update BindingAdapters.kt

--- app/src/main/java/com/example/sampledatabinding/BindingAdapters.kt
+++ app/src/main/java/com/example/sampledatabinding/BindingAdapters.kt
@@ -6,8 +6,8 @@ import androidx.databinding.BindingAdapter
 class BindingAdapters
 {
     @BindingAdapter("app:hideIfZero")
-        fun hideIfZero(view: View, number: Int)
-        {
-            view.visibility = if (number == 0) View.GONE else View.VISIBLE
-        }
+    fun hideIfZero(view: View, number: Int)
+    {
+        view.visibility = if (number == 0) View.GONE else View.VISIBLE
+    }
 }
